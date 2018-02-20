0ae78593c5424027df5285e8da16266603a0199e
Álvaro Alves <alvaro.a.alves@gmail.com>
2018-02-20 14:34:09 -0300

    Deletando funções ruins e Melhorando a principal

--- novo-slider.js
+++ novo-slider.js
@@ -5,15 +5,19 @@ var botaoPREV = document.querySelector(".botao-prev");
 
 var first = [0, 1, 2, 3, 4, 5];
 var last = [3, 4, 5, 0, 1, 2];
-var contador = 0;
+var contador = {
+    num:0,
+};
+var passou = true;
+
+var podeEntrar = false;
 
 botaoNEXT.addEventListener("click", next);
 botaoPREV.addEventListener("click", prev);
 
 
 function next(){
-
-        if(contador > sections.length - 1){
+        if(contador > sections.length - 1 || contador < 0){
             contador = 0;
         }
 
@@ -23,23 +27,41 @@ function next(){
         sections[last[contador]].classList.remove("some-div");
         sections[last[contador]].classList.add("aparece-div");
 
-        console.log(first[contador], last[contador]);
-        console.log(sections);
+        if(contador == 0){
+            podeEntrar = true;
+        }
 
         contador++;
-        console.log(contador);
+        console.log("CONTADOR: " + contador);
 }
 
 function prev(){
+    if(contador > sections.length - 1){
+        contador = 0;
+    }
 
-    var position = sections.length - (contador + 1);
-    console.log("position: " + position);
     console.log("contador antes: " + contador);
 
-    if(contador <= 0){
+    if(contador < 0){
+        passou = true;
+    }
+
+    if(contador <= 0 && passou){
+        contador = 0;
         contador = (sections.length - 1) + contador;
         console.log("contador depois: " + contador);
+
+        passou = false;
+
+        console.log(passou);
+        console.log("heheh to entrando nessa condicional ainda");
+
+        podeEntrar = true;
     }
+
+    if(contador <=0 || podeEntrar){
+        console.log("entrei nesse if");
+        console.log("o contador chega desse jeito: " + contador);
 
         sections[last[contador]].classList.remove("aparece-div");
         sections[last[contador]].classList.add("some-div");
@@ -48,15 +70,13 @@ function prev(){
         sections[first[contador]].classList.remove("some-div");
         sections[first[contador]].classList.add("aparece-div");
 
-        contador--;
         console.log("contador final: " + contador);
 
+        contador--;
+        console.log("CONTADOR DPS DE TUDO: " + contador);
         return;
 
-
-
-
-
+        }
 
 }
 
